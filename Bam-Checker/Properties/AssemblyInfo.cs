1b6d6f602aa246b153f1cea0bb3dae3c8d95d411
TechG2 <errorersplai@gmail.com>
2025-01-22 14:52:51 +0100

    fixed version

--- Bam-Checker/Properties/AssemblyInfo.cs
+++ Bam-Checker/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Windows;
 
 [assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
